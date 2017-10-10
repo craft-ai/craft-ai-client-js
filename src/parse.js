c3f45d2cd0b7bb4cac0fa99ffc432ee7d1de0fa9
Clodéric Mars <cloderic.mars@craft.ai>
2017-10-10 14:29:57 +0200

    Use proper craft ai errors in 'parse'

--- src/parse.js
+++ src/parse.js
@@ -1,23 +1,24 @@
 import _ from 'lodash';
 import semver from 'semver';
+import { CraftAiDecisionError } from './errors';
 
 export default function parse(input) {
   const json = _.isObject(input) ? input : JSON.parse(input);
   if (!_.isObject(json) || _.isArray(input)) {
-    throw new Error('Invalid decision tree format, the given json is not an object.');
+    throw new CraftAiDecisionError('Invalid decision tree format, the given json is not an object.');
   }
   if (_.isUndefined(json) || _.isUndefined(json._version)) {
-    throw new Error('Invalid decision tree format, unable to find the version informations.');
+    throw new CraftAiDecisionError('Invalid decision tree format, unable to find the version informations.');
   }
 
   const version = json._version;
   if (!semver.valid(version)) {
-    throw new Error(`Invalid decision tree format, "${version}" is not a valid version.`);
+    throw new CraftAiDecisionError(`Invalid decision tree format, "${version}" is not a valid version.`);
   }
   else if (semver.satisfies(version, '1.x')) {
     return json;
   }
   else {
-    throw new Error(`Invalid decision tree format, "${version}" is not a supported version.`);
+    throw new CraftAiDecisionError(`Invalid decision tree format, "${version}" is not a supported version.`);
   }
 }
