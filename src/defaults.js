d1d0c80860e331cc7076f99e6b5ac0b0c9d6f226
Clodéric Mars <cloderic.mars@craft.ai>
2018-11-27 11:05:17 +0100

    Increase the default timeout to 10 minutes

--- src/defaults.js
+++ src/defaults.js
@@ -1,7 +1,7 @@
 const DEFAULTS = {
   token: process.env.CRAFT_TOKEN,
   operationsChunksSize: 500,
-  decisionTreeRetrievalTimeout: 1000 * 60 * 5 // 5 minutes
+  decisionTreeRetrievalTimeout: 1000 * 60 * 10 // 10 minutes
 };
 
 export default DEFAULTS;
