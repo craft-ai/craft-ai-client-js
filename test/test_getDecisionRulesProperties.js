7eca5a55dac2a36b658027d9dbeb3763770a5b93
Anthony <anthony.damato@hotmail.fr>
2019-02-19 14:23:11 +0100

    add v2 tests in the getDecisionRulesProperties function

--- test/test_getDecisionRulesProperties.js
+++ test/test_getDecisionRulesProperties.js
@@ -1,24 +1,31 @@
 import { interpreter } from '../src';
 
-const DECISION_TREE_1 = require('./data/interpreter-test-suite/decide/trees/v1/enum.json');
-const DECISION_TREE_2 = require('./data/interpreter-test-suite/decide/trees/v1/continuous.json');
-const DECISION_TREE_3 = require('./data/interpreter-test-suite/decide/trees/v1/oneColor.json');
+const DECISION_TREE_1_V1 = require('./data/interpreter-test-suite/decide/trees/v1/enum.json');
+const DECISION_TREE_2_V1 = require('./data/interpreter-test-suite/decide/trees/v1/continuous.json');
+const DECISION_TREE_3_V1 = require('./data/interpreter-test-suite/decide/trees/v1/oneColor.json');
+
+const DECISION_TREE_1_V2 = require('./data/interpreter-test-suite/decide/trees/v2/enum.json');
+const DECISION_TREE_2_V2 = require('./data/interpreter-test-suite/decide/trees/v2/continuous.json');
+const DECISION_TREE_3_V2 = require('./data/interpreter-test-suite/decide/trees/v2/oneColor.json');
+const DECISION_TREE_4_V2 = require('./data/interpreter-test-suite/decide/trees/v2/optional_branch_enum.json');
 
 const INVALID_DECISION_TREE_1 = require('./data/interpreter-test-suite/decide/trees/v1/emptyArray.json');
 
 describe('interpreter.getDecisionRulesProperties(<tree>)', function() {
   it('can list the properties used in a tree\'s decision rules (enum output)', function() {
-    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_1)).to.be.deep.equal([
+    const expected_output = [
       {
         'is_generated': true,
         'property': 'timeOfDay',
         'type': 'time_of_day'
       }
-    ]);
+    ];
+    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_1_V1)).to.be.deep.equal(expected_output);
+    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_1_V2)).to.be.deep.equal(expected_output);
   });
 
   it('can list the properties used in a tree\'s decision rules (continuous output)', function() {
-    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_2)).to.be.deep.equal([
+    const expected_output = [
       {
         'is_generated': true,
         'property': 'time',
@@ -33,11 +40,72 @@ describe('interpreter.getDecisionRulesProperties(<tree>)', function() {
         'property': 'tz',
         'type': 'timezone'
       }
+    ];
+    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_2_V1)).to.be.deep.equal(expected_output);
+    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_2_V2)).to.be.deep.equal(expected_output);
+  });
+
+  it('can list the properties used in a tree\'s decision rules (optional property)', function() {
+    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_4_V2)).to.be.deep.equal([
+      {
+        'property': 'enum4',
+        'type': 'enum'
+      },
+      {
+        'property': 'continuous3',
+        'type': 'continuous'
+      },
+      {
+        'property': 'continuous5',
+        'type': 'continuous'
+      },
+      {
+        'property': 'continuous4',
+        'type': 'continuous'
+      },
+      {
+        'property': 'enum1',
+        'type': 'enum'
+      },
+      {
+        'property': 'enum5',
+        'type': 'enum'
+      },
+      {
+        'is_optional': true,
+        'property': 'enum2',
+        'type': 'enum'
+      },
+      {
+        'property': 'enum3',
+        'type': 'enum'
+      },
+      {
+        'property': 'enum6',
+        'type': 'enum'
+      },
+      {
+        'property': 'continuous2',
+        'type': 'continuous'
+      },
+      {
+        'property': 'continuous6',
+        'type': 'continuous'
+      },
+      {
+        'property': 'enum7',
+        'type': 'enum'
+      },
+      {
+        'property': 'continuous1',
+        'type': 'continuous'
+      }
     ]);
   });
 
   it('can list the properties used in a tree\'s decision rules (single leaf)', function() {
-    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_3)).to.be.deep.equal([]);
+    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_3_V1)).to.be.deep.equal([]);
+    expect(interpreter.getDecisionRulesProperties(DECISION_TREE_3_V2)).to.be.deep.equal([]);
   });
 
   it('works properly on invalid trees', function() {
