519b593edd0ed8316dc8af856e0efd92cd34a59c
Gaetan Millerand <>
2020-02-20 10:26:28 +0100

    Move agentName in ctxt ops to agent_id

--- test/test_getGeneratorContextOperations.js
+++ test/test_getGeneratorContextOperations.js
@@ -35,11 +35,11 @@ describe('client.getGeneratorContextOperations(<generatorId>, from, to)', functi
   it('should retrieve all context operations', function() {
     return client.getGeneratorContextOperations(generatorId)
       .then((operations) => {
-        expect(_.first(operations)).to.be.deep.equal({ agentName: agentId2, context: _.first(CONFIGURATION_1_OPERATIONS_2).context, timestamp: _.first(CONFIGURATION_1_OPERATIONS_2).timestamp });
-        expect(_.last(operations)).to.be.deep.equal({ agentName: agentId2, context: _.last(CONFIGURATION_1_OPERATIONS_2).context, timestamp: _.last(CONFIGURATION_1_OPERATIONS_2).timestamp });
+        expect(_.first(operations)).to.be.deep.equal({ agent_id: agentId2, context: _.first(CONFIGURATION_1_OPERATIONS_2).context, timestamp: _.first(CONFIGURATION_1_OPERATIONS_2).timestamp });
+        expect(_.last(operations)).to.be.deep.equal({ agent_id: agentId2, context: _.last(CONFIGURATION_1_OPERATIONS_2).context, timestamp: _.last(CONFIGURATION_1_OPERATIONS_2).timestamp });
         expect(operations.length).to.be.equal(CONFIGURATION_1_OPERATIONS_2.length + CONFIGURATION_1_OPERATIONS_1.length);
         const formattedExpectedResult = CONFIGURATION_1_OPERATIONS_2.slice(0, 1000)
-          .map((operation) => ({ agentName: agentId2, context: operation.context, timestamp: operation.timestamp }));
+          .map((operation) => ({ agent_id: agentId2, context: operation.context, timestamp: operation.timestamp }));
         expect(operations.slice(0, 1000)).to.be.deep.equal(formattedExpectedResult);
       });
   });
@@ -48,13 +48,13 @@ describe('client.getGeneratorContextOperations(<generatorId>, from, to)', functi
     return client.getGeneratorContextOperations(generatorId, lowerBound)
       .then((operations) => {
         const expectedOperations2 = _.filter(CONFIGURATION_1_OPERATIONS_2, ({ timestamp }) => timestamp >= lowerBound);
-        expect(operations[0]).to.be.deep.equal({ agentName: agentId2, context: expectedOperations2[0].context, timestamp: expectedOperations2[0].timestamp });
-        expect(operations[1]).to.be.deep.equal({ agentName: agentId, context: CONFIGURATION_1_OPERATIONS_1[1].context, timestamp: CONFIGURATION_1_OPERATIONS_1[1].timestamp });
-        expect(operations[2]).to.be.deep.equal({ agentName: agentId2, context: expectedOperations2[1].context, timestamp: expectedOperations2[1].timestamp });
-        expect(operations[3]).to.be.deep.equal({ agentName: agentId2, context: expectedOperations2[2].context, timestamp: expectedOperations2[2].timestamp });
+        expect(operations[0]).to.be.deep.equal({ agent_id: agentId2, context: expectedOperations2[0].context, timestamp: expectedOperations2[0].timestamp });
+        expect(operations[1]).to.be.deep.equal({ agent_id: agentId, context: CONFIGURATION_1_OPERATIONS_1[1].context, timestamp: CONFIGURATION_1_OPERATIONS_1[1].timestamp });
+        expect(operations[2]).to.be.deep.equal({ agent_id: agentId2, context: expectedOperations2[1].context, timestamp: expectedOperations2[1].timestamp });
+        expect(operations[3]).to.be.deep.equal({ agent_id: agentId2, context: expectedOperations2[2].context, timestamp: expectedOperations2[2].timestamp });
         expect(operations.length).to.equal(expectedOperations2.length + CONFIGURATION_1_OPERATIONS_1.length - 1);
         const formattedExpectedResult = _.takeRight(expectedOperations2, 100)
-          .map((operation) => ({ agentName: agentId2, context: operation.context, timestamp: operation.timestamp }));
+          .map((operation) => ({ agent_id: agentId2, context: operation.context, timestamp: operation.timestamp }));
         expect(_.takeRight(operations, 100)).to.be.deep.equal(formattedExpectedResult);
       });
   });
@@ -64,13 +64,13 @@ describe('client.getGeneratorContextOperations(<generatorId>, from, to)', functi
     return client.getGeneratorContextOperations(generatorId, undefined, 1464600406)
       .then((operations) => {
         const expectedOperations2 = _.filter(CONFIGURATION_1_OPERATIONS_2, ({ timestamp }) => timestamp <= upperBound);
-        expect(operations[operations.length - 1]).to.be.deep.equal({ agentName: agentId2, context: expectedOperations2[expectedOperations2.length - 1].context, timestamp: expectedOperations2[expectedOperations2.length - 1].timestamp });
-        expect(operations[operations.length - 2]).to.be.deep.equal({ agentName: agentId2, context: expectedOperations2[expectedOperations2.length - 2].context, timestamp: expectedOperations2[expectedOperations2.length - 2].timestamp });
-        expect(operations[operations.length - 3]).to.be.deep.equal({ agentName: agentId, context: CONFIGURATION_1_OPERATIONS_1[0].context, timestamp: CONFIGURATION_1_OPERATIONS_1[0].timestamp });
-        expect(operations[operations.length - 4]).to.be.deep.equal({ agentName: agentId2, context: expectedOperations2[expectedOperations2.length - 3].context, timestamp: expectedOperations2[expectedOperations2.length - 3].timestamp });
+        expect(operations[operations.length - 1]).to.be.deep.equal({ agent_id: agentId2, context: expectedOperations2[expectedOperations2.length - 1].context, timestamp: expectedOperations2[expectedOperations2.length - 1].timestamp });
+        expect(operations[operations.length - 2]).to.be.deep.equal({ agent_id: agentId2, context: expectedOperations2[expectedOperations2.length - 2].context, timestamp: expectedOperations2[expectedOperations2.length - 2].timestamp });
+        expect(operations[operations.length - 3]).to.be.deep.equal({ agent_id: agentId, context: CONFIGURATION_1_OPERATIONS_1[0].context, timestamp: CONFIGURATION_1_OPERATIONS_1[0].timestamp });
+        expect(operations[operations.length - 4]).to.be.deep.equal({ agent_id: agentId2, context: expectedOperations2[expectedOperations2.length - 3].context, timestamp: expectedOperations2[expectedOperations2.length - 3].timestamp });
         expect(operations.length).to.equal(expectedOperations2.length + 1);
         const formattedExpectedResult = _.take(expectedOperations2, 1000)
-          .map((operation) => ({ agentName: agentId2, context: operation.context, timestamp: operation.timestamp }));
+          .map((operation) => ({ agent_id: agentId2, context: operation.context, timestamp: operation.timestamp }));
         expect(_.take(operations, 1000)).to.be.deep.equal(formattedExpectedResult);
       });
   });
@@ -91,7 +91,7 @@ describe('client.getGeneratorContextOperations(<generatorId>, from, to)', functi
     return client.getGeneratorContextOperations(generatorId, lowerBound, upperBound)
       .then((operations) => {
         const expectedOperations = _.filter(CONFIGURATION_1_OPERATIONS_2, ({ timestamp }) => timestamp >= lowerBound && timestamp <= upperBound);
-        expect(operations).to.be.deep.equal(expectedOperations.map((operation) => ({ agentName: agentId2, context: operation.context, timestamp: operation.timestamp })));
+        expect(operations).to.be.deep.equal(expectedOperations.map((operation) => ({ agent_id: agentId2, context: operation.context, timestamp: operation.timestamp })));
       });
   });
 });
