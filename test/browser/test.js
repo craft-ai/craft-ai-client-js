74720dce4520dc62bf5f7a0d13766a7d75481f13
rmdh <2242623+rmdh@users.noreply.github.com>
2020-02-20 01:29:25 +0100

    Add generator tests to browser tests

--- test/browser/test.js
+++ test/browser/test.js
@@ -2,10 +2,12 @@ require('./helper');
 
 // require('../test_interpreterSuite'); // Not suitable for browser
 require('../test_addAgentContextOperations');
+require('../test_computeGeneratorDecision');
 require('../test_computeAgentDecision');
 require('../test_context');
 require('../test_createAgent');
 require('../test_createClient');
+require('../test_createGenerator');
 require('../test_decideFromContextsArray');
 require('../test_errors');
 require('../test_getAgent');
@@ -13,8 +15,12 @@ require('../test_getAgentContextOperations');
 require('../test_getAgentDecisionTree');
 require('../test_getAgentStateHistory');
 require('../test_getDecisionRulesProperties');
+require('../test_getGenerator');
+require('../test_getGeneratorContextOperations');
+require('../test_getGeneratorDecisionTree');
 require('../test_getSharedAgentInspectorUrl');
 require('../test_listAgents');
+require('../test_listGenerators');
 require('../test_properties');
 require('../test_time');
 
